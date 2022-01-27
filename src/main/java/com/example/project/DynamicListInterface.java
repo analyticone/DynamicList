bd629d863c46b1568ef78bf4a3c486c263bd4dae
analyticone <steven.kreutzer@yahoo.com>
2022-01-27 00:01:33 -0500

    Update DynamicListInterface.java
    removed replace and replaceAll

--- src/main/java/com/example/project/DynamicListInterface.java
+++ src/main/java/com/example/project/DynamicListInterface.java
@@ -4,26 +4,15 @@ public interface DynamicListInterface<Item>
 {
 	public void add(Item item);
 	public void addAt(int index, Item item);
-
 	public Item get(int index);
-
-	public Item set(int index, Item item);
-
-	public boolean replace(Item oldItem, Item newItem);
-	public boolean replaceAll(Item oldItem, Item newItem);
-	
+	public Item set(int index, Item item);	
 	public boolean remove(Item item);
 	public Item removeAt(int index);
-		
 	public int indexOf(Item item);	
 	public int lastIndexOf(Item item);
-	
 	public boolean contains(Item item);
-
 	public int size();
 	public boolean isEmpty();
-	
 	public void clear();
-	
 	public Iterator<Item> iterator();
 }
